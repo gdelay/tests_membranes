23489061778a36ddf61af31f13048b4e5c3bffe2
gdelay <gui.delay@laposte.net>
2020-04-28 12:34:34 +0200

    corrected assembly

--- apps/membranes/membranes.cpp
+++ apps/membranes/membranes.cpp
@@ -1546,7 +1546,7 @@ public:
             }
         }
 
-        for (size_t i = 0; i < rhs.rows(); i++)
+        for (size_t i = 0; i < rhs_sc.rows(); i++)
         {
             if (!asm_map[i].assemble())
                 continue;
